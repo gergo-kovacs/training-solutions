3ed50a8dedc80ee0f200c788fe69f95ff8200c16
Gergo Kovacs <kovacsgergo.info@gmail.com>
2020-10-29 11:19:45 +0100

    lesson2 first commit

---
+++ src/main/java/classstructureio/Registration.java
@@ -0,0 +1,21 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Registration {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Kérem a nevét: ");
+        String name;
+        name = scanner.nextLine();
+
+        System.out.print("Kérem az email címét: ");
+        String email;
+        email = scanner.nextLine();
+
+        System.out.println("Név: "+name);
+        System.out.println("Email: "+email);
+    }
+}
