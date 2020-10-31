9b2589bc8fc9d208def42686e57b48863fd06c4a
Gergo Kovacs <kovacsgergo.info@gmail.com>
2020-10-31 19:40:22 +0100

    Uml,példányok 2.feladat(Bank) 1.commit

---
+++ src/main/java/classsctructureintegrate/Bank.java
@@ -0,0 +1,20 @@
+package classsctructureintegrate;
+
+public class Bank {
+
+    public static void main(String[] args) {
+
+    BankAccount OtpAccountKP = new BankAccount("10073217-12000098-67341590","Kis Pista", 50000);
+    BankAccount MkbAccountNA = new BankAccount("24523417-00100055-23453451","Nagy Anna",100000);
+
+    System.out.println(OtpAccountKP.getInfo());
+    System.out.println(MkbAccountNA.getInfo());
+    OtpAccountKP.transfer(MkbAccountNA,49999);
+    System.out.println(OtpAccountKP.getInfo()+" "+MkbAccountNA.getInfo());
+    OtpAccountKP.deposit(5000);
+    MkbAccountNA.withdraw(3000);
+    System.out.println(OtpAccountKP.getInfo()+" "+MkbAccountNA.getInfo());
+
+    }
+
+}
---
+++ src/main/java/classsctructureintegrate/BankAccount.java
@@ -0,0 +1,34 @@
+package classsctructureintegrate;
+
+public class BankAccount {
+
+    private final String accountNumber;
+    private final String owner;
+    private int balance;
+
+    public BankAccount(String accountNumber, String owner, int balance) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public void deposit(int amount){
+        this.balance += amount;
+    }
+
+    public void withdraw(int amount){
+        this.balance -= amount;
+    }
+
+    public void transfer(BankAccount to,int amount){
+    to.balance += amount;
+    this.balance -= amount;
+    }
+
+    public String getInfo(){
+        return this.owner+" ("+this.accountNumber+"): "+this.balance+" Ft";
+    }
+
+
+
+}
