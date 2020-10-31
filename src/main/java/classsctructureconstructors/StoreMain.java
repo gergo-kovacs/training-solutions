482ae30124530279853bd745834ac7aba2218e23
Gergo Kovacs <kovacsgergo.info@gmail.com>
2020-10-31 18:56:30 +0100

    Konstruktorok 2.feladat(Raktár) 1.commit

---
+++ src/main/java/classsctructureconstructors/StoreMain.java
@@ -0,0 +1,17 @@
+package classsctructureconstructors;
+
+public class StoreMain {
+    public static void main(String[] args) {
+
+        Store raktar1 = new Store("ware1");
+        Store raktar2 = new Store("ware2");
+        System.out.println("Raktar1: "+raktar1.getStock()+" Raktar2: "+raktar2.getStock());
+        raktar1.store(2);
+        raktar2.store(4);
+        System.out.println("Raktar1: "+raktar1.getStock()+" Raktar2: "+raktar2.getStock());
+        raktar2.dispatch(1);
+        System.out.println("Raktar1: "+raktar1.getStock()+" Raktar2: "+raktar2.getStock());
+
+    }
+
+}
