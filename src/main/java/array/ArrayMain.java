2b07f625b33ba776b4de6e85c3827f41cbebf865
Gergo Kovacs <kovacsgergo.info@gmail.com>
2020-11-03 19:16:33 +0100

    Tömbök 1.feladat (Tömbök kezelése) 1.commit

---
+++ src/main/java/array/ArrayMain.java
@@ -0,0 +1,28 @@
+package array;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+        String[] theDaysOfWeek = {"Monday" ,"Tuesday" ,"Wednesday" ,"Thursday" ,"Friday" ,"Saturday" ,"Sunday "};
+        System.out.println(theDaysOfWeek[1]);
+
+        int[] n = new int[5];
+        for (int i = 0; i < n.length; i++) {
+            n[i] = (int) Math.pow(2,i);
+            System.out.print(n[i]+" ");
+        }
+
+        System.out.println();
+
+        boolean[] trueOrFalse = new boolean[6];
+
+        for (int i = 0; i < trueOrFalse.length; i++) {
+            if (i % 2 == 0 || i == 0) {
+                trueOrFalse[i] = true;
+            } else {
+                trueOrFalse[i] = false;
+            }
+            System.out.print(trueOrFalse[i] + " ");
+        }
+
+    }
+}
