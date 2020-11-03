cd11c68514cdd9e0861f3221262b57750541f955
Gergo Kovacs <kovacsgergo.info@gmail.com>
2020-11-03 12:10:08 +0100

    Literálok,lokális változók 1.feladat 1.commit

---
+++ src/main/java/localvariables/LocalVariablesMain.java
@@ -0,0 +1,19 @@
+package localvariables;
+
+public class LocalVariablesMain {
+    public static void main(String[] args) {
+        boolean b = false;
+        int a = 2;
+        int i = 3; int j = 4;
+        int k = i;
+        String s = "Hello World";
+        String t = s;
+        {
+            int x = 0;
+            System.out.println(x);
+            System.out.println(t);
+        }
+
+        //System.out.println(x);  //java: cannot find symbol
+    }
+}
