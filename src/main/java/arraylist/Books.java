0bbbf199c20591eafaa23ba4d03a246b255cb5bc
Gergo Kovacs <kovacsgergo.info@gmail.com>
2020-11-04 22:14:57 +0100

    Lista 2.commit

---
+++ src/main/java/arraylist/Books.java
@@ -0,0 +1,35 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Books {
+    List<String> titles = new ArrayList<>();
+
+    public void add(String book){
+        titles.add(book);
+    }
+
+    public List<String>findAllByPrefix(String prefix){
+        List<String> eureka = new ArrayList<>();
+        for (int i = 0; i < titles.size(); i++) {
+        if(titles.get(i).startsWith(prefix))
+            eureka.add(titles.get(i));
+        }
+        return eureka;
+    }
+
+    public List<String> getTitles(){
+        return titles;
+    }
+
+    public static void main(String[] args) {
+        Books books = new Books();
+        books.add("A madarak");
+        books.add("A kutyák");
+        books.add("A macskák");
+
+        System.out.println(books.getTitles());
+        System.out.println(books.findAllByPrefix("A ma"));
+    }
+}
