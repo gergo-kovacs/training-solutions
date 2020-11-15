b5befff0f6496c9fbc4eb1c917c32d65f3b250f1
gergo-kovacs <86kovacsgergo@gmail.com>
2020-11-15 19:37:50 +0100

    A hét napjai

---
+++ src/main/java/enumtype/week/WorkdayCalculator.java
@@ -0,0 +1,31 @@
+package enumtype.week;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WorkdayCalculator {
+    public List<DayType> dayTypes(Day firstDay, int numberOfDays) {
+
+        List<DayType> days = new ArrayList<>();
+        
+        Day parameters = firstDay;
+        for (int i = 0; i < numberOfDays; i++) {
+            
+            days.add(parameters.getDayType());
+            parameters = nextDay(parameters);
+        }
+
+        return days;
+    }
+
+    private Day nextDay(Day day) {
+        int index;
+        if(day.ordinal()==Day.values().length-1){
+            index = 0;
+        }else{
+            index = day.ordinal() + 1;
+        }
+        return Day.values()[index];
+    }
+}
