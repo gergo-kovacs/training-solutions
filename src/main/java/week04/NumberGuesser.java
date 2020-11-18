08c5b1a62fb25979da65cfdde6676e6a86a40125
gergo-kovacs <86kovacsgergo@gmail.com>
2020-11-18 13:29:55 +0100

    feladat

---
+++ src/main/java/week04/NumberGuesser.java
@@ -0,0 +1,33 @@
+package week04;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuesser {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int number = rand.nextInt(100) + 1;
+        boolean valasz = false;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Kérek egy számot!");
+        int num = scan.nextInt();
+
+        do {
+
+            if (num > number) {
+                System.out.println("Kisebb számra gondoltam.");
+                num =scan.nextInt();
+            } else if (num < number) {
+                System.out.println("Nagyobb számra gondoltam.");
+                num=scan.nextInt();
+            } else {
+                valasz=true;
+                System.out.println("Talált!");
+            }
+        } while (valasz != true);
+
+    }
+
+}
