fe442c5714df82419907275246b0b827da3b4afc
gergo-kovacs <86kovacsgergo@gmail.com>
2020-11-20 13:26:44 +0100

    feladat

---
+++ src/main/java/week04/Main.java
@@ -0,0 +1,24 @@
+package week04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.getIndexesOfChar("kefe",'e'));
+    }
+
+    List<Integer> getIndexesOfChar(String str, char c) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i)==c){
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+}
