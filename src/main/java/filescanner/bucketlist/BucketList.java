88b7b21bb3087770be27a8d1011dba2f0f4e9cf8
gergo-kovacs <86kovacsgergo@gmail.com>
2020-11-23 13:01:05 +0100

    bakancslista  gyökérkönyvtár

---
+++ src/main/java/filescanner/bucketlist/BucketList.java
@@ -0,0 +1,18 @@
+package filescanner.bucketlist;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class BucketList {
+    public static void main(String[] args) {
+        try(Scanner scanner = new Scanner(Path.of("bucketlist.txt"))){
+            while(scanner.hasNextLine()){
+                String row = scanner.nextLine();
+                System.out.println(row);
+            }
+        }catch (IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+}
