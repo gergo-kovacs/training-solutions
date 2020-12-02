bb9eda8668f2e7347bc992583b7d7f8868d7e830
gergo-kovacs <86kovacsgergo@gmail.com>
2020-12-02 15:53:34 +0100

    week06d03

--- src/main/java/week06d03/WordEraser.java
+++ src/main/java/week06d03/WordEraser.java
@@ -4,12 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WordEraser {
-    public static void main(String[] args) {
-        String s = "I want to walk my dog";
-        WordEraser we = new WordEraser();
 
-        System.out.println(we.eraseWord("alma almafa barack","alma"));
-    }
     public String eraseWord(String words, String word){
         if(isEmpty(words,word)){
             throw new IllegalArgumentException("Parameter must not be empty!");
