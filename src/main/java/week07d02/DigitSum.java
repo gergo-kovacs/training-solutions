95c8a41f6327728f0d2dc4e61a4b4d1fa7f89a9f
gergo-kovacs <86kovacsgergo@gmail.com>
2020-12-09 11:04:27 +0100

    week07d02 javított

--- src/main/java/week07d02/DigitSum.java
+++ src/main/java/week07d02/DigitSum.java
@@ -1,11 +1,15 @@
 package week07d02;
 
 
+import java.util.List;
+
 public class DigitSum {
 
+
+
     public int sumOfDigits(int x){
-     String numbers = String.valueOf(x);
-     int sum = 0;
+        String numbers = String.valueOf(x);
+        int sum = 0;
         for (int i = 0; i < numbers.length(); i++) {
             sum += numbers.charAt(i)-48;
         }
@@ -20,6 +24,16 @@ public class DigitSum {
         }
         return sum;
     }
+
+    public int sumOfDigits3(int x){
+        int sum = 0;
+        while (x!=0){
+            sum+=x%10;
+            x=x/10;
+        }
+        return sum;
+    }
+
 
 
 }
--- src/test/java/week07d02/DigitSumTest.java
+++ src/test/java/week07d02/DigitSumTest.java
@@ -2,12 +2,18 @@ package week07d02;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import stringconcat.stringconcat.Name;
+import stringconcat.stringconcat.Title;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DigitSumTest {
 
     @Test
     public void testSumODigits(){
-        Assertions.assertEquals(6,new DigitSum().sumOfDigits(123));
+        Assertions.assertEquals(8,new DigitSum().sumOfDigits(1025));
+        Assertions.assertEquals(5,new DigitSum().sumOfDigits2(32));
+        Assertions.assertEquals(3,new DigitSum().sumOfDigits3(003));
 
     }
 }
