cc6e4fd331e9a2a566bfeb5b9331e6d9fd26b43c
gergo-kovacs <86kovacsgergo@gmail.com>
2020-12-09 09:48:17 +0100

    week07d02 2

--- src/main/java/week07d02/DigitSum.java
+++ src/main/java/week07d02/DigitSum.java
@@ -5,10 +5,21 @@ public class DigitSum {
 
     public int sumOfDigits(int x){
      String numbers = String.valueOf(x);
-     int sum=0;
+     int sum = 0;
         for (int i = 0; i < numbers.length(); i++) {
             sum += numbers.charAt(i)-48;
         }
         return sum;
     }
+
+    public int sumOfDigits2(int x){
+        String numbers = String.valueOf(x);
+        int sum = 0;
+        for (int i = 0; i < numbers.length(); i++) {
+            sum += Integer.parseInt(String.valueOf(numbers.charAt(i)));
+        }
+        return sum;
+    }
+
+
 }
