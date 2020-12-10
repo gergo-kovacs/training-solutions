accdf438e868d38b062cb2489ccf7582382baa0a
gergo-kovacs <86kovacsgergo@gmail.com>
2020-12-10 10:21:54 +0100

    konstruktorok 1/1. javitas

--- src/main/java/defaultconstructor/date/SimpleDateFormatter.java
+++ src/main/java/defaultconstructor/date/SimpleDateFormatter.java
@@ -2,18 +2,19 @@ package defaultconstructor.date;
 
 public class SimpleDateFormatter {
 
-    private CountryCode cCode = CountryCode.HU;
+    private CountryCode defaultCode;
 
     public SimpleDateFormatter() {
+        this.defaultCode = CountryCode.HU;
     }
 
     public String formatDateString(SimpleDate simpleDate){
-       return simpleDate.getYear()+"-"+simpleDate.getMonth()+"-"+simpleDate.getDay();
+        return formatDateStringByCountryCode(defaultCode,simpleDate);
     }
 
     public String formatDateStringByCountryCode(CountryCode countryCode, SimpleDate simpleDate){
-        if(cCode.name().equals(countryCode.name())){
-            return formatDateString(simpleDate);
+        if(countryCode.name().equals(CountryCode.HU.name())){
+            return simpleDate.getYear()+"-"+simpleDate.getMonth()+"-"+simpleDate.getDay();
         }else if(countryCode.name().equals(CountryCode.EN.name())){
             return simpleDate.getDay()+"-"+simpleDate.getMonth()+"-"+simpleDate.getYear();
         }else{
