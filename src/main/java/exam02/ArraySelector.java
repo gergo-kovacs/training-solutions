5fccdcb24d16e2c8720f19258e2da186b7504a84
gergo-kovacs <86kovacsgergo@gmail.com>
2021-01-06 20:37:08 +0100

    exam02 1. feladat

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -1,4 +1,28 @@
 package exam02;
 
 public class ArraySelector {
+
+    public String selectEvens(int[] array){
+        if(isEmpty(array)){
+            return "";
+        }
+        String result = "[";
+        if(array.length==1){
+            return result+array[0]+"]";
+        }
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]%2!=0){
+                result += array[i];
+                result +=", ";
+            }
+        }
+        result=result.substring(0,result.length()-2);
+        return result+"]";
+    }
+
+    private boolean isEmpty(int[] array){
+        return array.length==0;
+    }
+
+
 }
