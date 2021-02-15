f3d79619b94ebc0165bfc76a0bc21dc22b031c7e
gergo-kovacs <86kovacsgergo@gmail.com>
2021-02-15 18:45:11 +0100

    1.feladat

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,7 +1,16 @@
 package exam03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 //Hisztogram
 //
-//Olvass be egy szöveges fájlt soronként, melyben soronkont számok vannak. Majd hozz létre egy stringet, mely ugyanennyi sort tartalmaz, és annyi csillagot ír ki egymás mellé soronkont, amennyi a bemeneti fájlban lévő szám.
+//Olvass be egy szöveges fájlt soronként, melyben soronkont számok vannak. Majd hozz létre egy stringet,
+// mely ugyanennyi sort tartalmaz, és annyi csillagot ír ki egymás mellé soronkont, amennyi a bemeneti
+// fájlban lévő szám.
 //
 //Azaz a bemeneti fájl:
 //
@@ -21,5 +30,34 @@ package exam03;
 //
 //A Histogram osztályba dolgozz, tesztje HistogramTest. Minden sor után legyen sortörés, az utolsó után is.
 public class Histogram {
+
+    public String createHistogram(BufferedReader reader) throws IOException {
+        String line;
+        String result="";
+        while ((line = reader.readLine())  != null) {
+            //System.out.println(line);
+            result += word(line);
+            result+="\n";
+        }
+        return result;
+    }
+
+    private StringBuilder word(String text){
+        StringBuilder stars = new StringBuilder();
+        for (int i = 0; i < Integer.parseInt(text); i++) {
+            stars.append("*");
+        }
+        System.out.println(stars);
+        return stars;
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = Files.newBufferedReader(Path.of("histogram.txt"))) {
+            new Histogram().createHistogram(reader);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+
+    }
 
 }
